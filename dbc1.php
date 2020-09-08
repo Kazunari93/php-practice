494c38a77f280a3e5521899c804a58bcb16e0cd5
Kazunari93 <aooni0930@gmail.com>
2020-09-08 22:42:25 +0900

    データベースの表示

--- dbc1.php
+++ dbc1.php
@@ -4,6 +4,7 @@ $dsn = 'dsn'; //my dsn
 $user = 'user'; // my user
 $pass = 'pass'; // my password 
 
+
 try {
   $dbh = new PDO(
     $dsn,
@@ -11,9 +12,45 @@ try {
     $pass,
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
   );
-  echo '成功';
+  //echo '成功';
+
+  $sql = 'SELECT * FROM blog';
+  //SQL実行
+  $stmt = $dbh->query($sql);
+  //結果を受け取る
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
   $dbh = null;
 } catch (PDOException $e) {
   echo '失敗' . $e->getMessage();
   exit();
 };
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <table>
+    <tr>
+      <th>No</th>
+      <th>タイトル</th>
+      <th>カテゴリー</th>
+    </tr>
+    <?php foreach ($result as $column) : ?>
+      <tr>
+        <td><?php echo $column['id'] ?></td>
+        <td><?php echo $column['title'] ?></td>
+        <td><?php echo $column['category'] ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
