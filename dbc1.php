6f38208baf3ca182f6bba6162ceb4dcd101c9615
Kazunari93 <aooni0930@gmail.com>
2020-09-13 19:13:43 +0900

    refactoring

--- dbc1.php
+++ dbc1.php
@@ -1,30 +1,52 @@
 <?php
 
-$dsn = 'dsn'; //my dsn
-$user = 'user'; // my user
-$pass = 'pass'; // my password 
 
+function dbConnect()
+{
+  $dsn = 'dsn'; //my dsn
+  $user = 'user'; // my user
+  $pass = 'pass'; // my password 
 
-try {
-  $dbh = new PDO(
-    $dsn,
-    $user,
-    $pass,
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-  );
-  //echo '成功';
+  try {
+    $dbh = new PDO(
+      $dsn,
+      $user,
+      $pass,
+      [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+  } catch (PDOException $e) {
+    echo '失敗' . $e->getMessage();
+    exit();
+  };
 
+  return $dbh;
+}
+
+function getAllBlog()
+{
+  $dbh = dbConnect();
   $sql = 'SELECT * FROM blog';
   //SQL実行
   $stmt = $dbh->query($sql);
   //結果を受け取る
   $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+  return $result;
   $dbh = null;
-} catch (PDOException $e) {
-  echo '失敗' . $e->getMessage();
-  exit();
-};
+}
+
+$data = getAllBlog();
+
+function setCategory($num)
+{
+  if ($num === '1') {
+    return 'ブログ';
+  } elseif ($num === '2') {
+    return '趣味';
+  } else {
+    return 'その他';
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -43,11 +65,11 @@ try {
       <th>タイトル</th>
       <th>カテゴリー</th>
     </tr>
-    <?php foreach ($result as $column) : ?>
+    <?php foreach ($data as $column) : ?>
       <tr>
         <td><?php echo $column['id'] ?></td>
         <td><?php echo $column['title'] ?></td>
-        <td><?php echo $column['category'] ?></td>
+        <td><?php echo setCategory($column['category']) ?></td>
       </tr>
     <?php endforeach; ?>
   </table>
